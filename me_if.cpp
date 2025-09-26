53f9e615419eb7cc36522510d350ea32547efcdd
Yacinnegha <151468394+Yacinnegha@users.noreply.github.com>
2025-09-26 08:46:59 +0100

    Create me_if.cpp
    Added if statment to check age

---
+++ me_if.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int age = 20;
+    cout << "I am " << age << " years old";
+    if (age >= 20) {
+        cout << " and ready to code!";
+    } else {
+        cout << " and still learning!";
+    }
+}
